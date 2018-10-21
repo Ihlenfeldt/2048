9efde81066a26c4bbccb84b3cf8c159ba3241712
Jenny Meadowcroft <jmeadowcroft@carroll.edu>
2018-10-21 12:22:37 -0600

    Updated Block with Coordinates

--- Block2048.java
+++ Block2048.java
@@ -2,6 +2,8 @@ package game2048;
 
 public class Block2048 {
 	int blockValue;
+	int xCoord;
+	int yCoord;
 
 	public Block2048(int passedValue){
 		this.blockValue = passedValue;
@@ -12,7 +14,23 @@ public class Block2048 {
 	public void setBlockValue(int passedValue) {
 		this.blockValue = passedValue;
 	}
+	public int getX()
+	{
+		return xCoord;
+	}
+	public int getY()
+	{
+		return yCoord;
+	}
 	
+	public void setX(int x)
+	{
+		xCoord = x;
+	}
+	public void setY(int y)
+	{
+		yCoord = y;
+	}
 	public boolean isMoving() {
 		return false;
 	}
