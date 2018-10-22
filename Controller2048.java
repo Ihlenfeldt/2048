0ee478d8e8126ad077f4467e3c6b29c690f5da67
James Ihlenfeldt <jihlenfeldt@carroll.edu>
2018-10-22 15:27:26 -0600

    Added KeyListener Switch Statement and gameType Variable in Controller Class

--- Controller2048.java
+++ Controller2048.java
@@ -21,9 +21,30 @@ public class Controller2048 extends TimerTask implements MouseListener, KeyListe
 	public static final int ARRAY_WIDTH = 8;
 	public static final int ARRAY_HEIGHT = ARRAY_WIDTH;
 	
+	//Key_Codes for keyListener below
+	public static final int UP_ARROW = 38;
+	public static final int DOWN_ARROW = 40;
+	public static final int RIGHT_ARROW = 39;
+	public static final int LEFT_ARROW = 37;
+	public static final int NUMPAD_6 = 102;
+	public static final int NUMPAD_5 = 0;
+	public static final int NUMPAD_4 = 100;
+	public static final int NUMPAD_2 = 98;
+	public static final int NUMPAD_8 = 104;
+	public static final int KEY_W = 87;
+	public static final int KEY_D = 68;
+	public static final int KEY_S = 83;
+	public static final int KEY_A = 65;
+	public static final int KEY_SPACE = 32;
+	public static final int KEY_ESC = 27;
+
+	
 	public static final int TIME_BETWEEN_MOVES = 700;//Time = 0.7 seconds
 	public static final int MAX_TIME_TO_MOVE = 6000;//Time = 6 seconds.
 	public static long currentTime = System.currentTimeMillis();//This will be used to track max time between moves
+	
+	//Keeps track of game type Original = 0, Russian = 1
+	private int gameType = 0;
 	
 	private JFrame gameFrame2048;
 	private Container contentPane2048;
@@ -44,6 +65,7 @@ public class Controller2048 extends TimerTask implements MouseListener, KeyListe
 		contentPane2048 = gameFrame2048.getContentPane();
 		contentPane2048.setLayout(null);
 		contentPane2048.setBackground(Color.GRAY);
+		
 		gameFrame2048.setVisible(true);
 
 		
@@ -51,7 +73,7 @@ public class Controller2048 extends TimerTask implements MouseListener, KeyListe
 		
 		contentPane2048.addMouseListener(this);
 		contentPane2048.addKeyListener(this);
-		
+		contentPane2048.setFocusable(true);
 		
 	}
 	
@@ -72,7 +94,7 @@ public class Controller2048 extends TimerTask implements MouseListener, KeyListe
 		}
 
 	@Override
-	public void mouseClicked(MouseEvent arg0) {
+	public void mouseClicked(MouseEvent e) {
 		// TODO Auto-generated method stub
 		
 	}
@@ -102,9 +124,99 @@ public class Controller2048 extends TimerTask implements MouseListener, KeyListe
 	}
 
 	@Override
-	public void keyPressed(KeyEvent arg0) {
+	public void keyPressed(KeyEvent e) {
 		// TODO Auto-generated method stub
-		
+		int keyPressed_Code = e.getKeyCode();
+		if(gameType == 0) {
+			switch(keyPressed_Code) {
+			case UP_ARROW: System.out.println("Up Arrow");
+			break;
+			
+			case DOWN_ARROW: System.out.println("Down Arrow");
+			break;
+			
+			case RIGHT_ARROW:System.out.println("Right Arrow");
+			break;
+			
+			case LEFT_ARROW:System.out.println("Left Arrow");
+			break;
+			
+			case NUMPAD_6:
+			break;
+			
+			case NUMPAD_8:
+			break;
+			
+			case NUMPAD_4:
+			break;
+			
+			case NUMPAD_2:
+			break;
+			
+			case KEY_W:
+			break;
+			
+			case KEY_D:
+			break;
+			
+			case KEY_S:
+			break;
+			
+			case KEY_A:
+			break; 
+			
+			case KEY_SPACE:
+			break;
+			
+			case KEY_ESC:
+			break;
+			}
+		}
+		else{
+			switch(keyPressed_Code) {
+			case UP_ARROW: System.out.println("Up Arrow");
+			break;
+			
+			case DOWN_ARROW: System.out.println("Down Arrow");
+			break;
+			
+			case RIGHT_ARROW:
+			break;
+			
+			case LEFT_ARROW:
+			break;
+			
+			case NUMPAD_6:
+			break;
+			
+			case NUMPAD_8:
+			break;
+			
+			case NUMPAD_4:
+			break;
+			
+			case NUMPAD_2:
+			break;
+			
+			case KEY_W:
+			break;
+			
+			case KEY_D:
+			break;
+			
+			case KEY_S:
+			break;
+			
+			case KEY_A:
+			break; 
+			
+			case KEY_SPACE:
+			break;
+			
+			case KEY_ESC:
+			break;
+			}
+		}
 	}
 
 	@Override
